c92d4d316161995a64bf5e1b44cb21ca0a3574b8
Stefan Braunovic <stefan.braunovic55@gmail.com>
2021-09-16 10:46:08 +0200

    prvih deset

---
+++ ex04/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+
+    var  myDecimal=1.2;
+
+    var myDecimalE= 0.00254;
+
+    return{
+        myDecimal,
+        myDecimalE
+    }
+
+
+}
+
+
+console.log(main());
+
+module.exports=main;
---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+var myFirstName="Stefan";
+var myFirstLast="Braunovic";
+var myHomeTown="Podgorica";
+
+    return{
+        myFirstName,
+        myLastName,
+        myHomeTown
+    }
+}
+
+
+console.log(main());
+
+module.exports=main;
